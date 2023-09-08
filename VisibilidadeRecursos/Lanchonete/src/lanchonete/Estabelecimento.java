f91d1d0d4f3ee3d2139760f2a0c190cb936eead5
Bruno Henrique Marinho Marques <brunohenriw1@gmail.com>
2023-09-08 18:17:11 -0300

    alteração na visibilidade dos métodos.

--- VisibilidadeRecursos/Lanchonete/src/lanchonete/Estabelecimento.java
+++ VisibilidadeRecursos/Lanchonete/src/lanchonete/Estabelecimento.java
@@ -7,13 +7,6 @@ import lanchonete.atendimento.*;
 public class Estabelecimento {
 	public static void main(String[] args) {
 		Cozinheiro cozinheiro = new Cozinheiro();
-		//ações que não precisam estarem disponíveis para toda a aplicação
-		cozinheiro.lavarIngredientes();
-		cozinheiro.baterVitaminaLiquidificador();
-		cozinheiro.selecionarIngredientesVitamina();
-		cozinheiro.prepararLanche();
-		cozinheiro.prepararVitamina();
-		cozinheiro.prepararVitamina();
 		
 		//ações que estabelecimento precisa ter ciência
 		cozinheiro.adicionarSucoNoBalcao();;
@@ -30,8 +23,7 @@ public class Estabelecimento {
 		atendente.pegarLancheCozinha();
 		atendente.receberPagamento();
 		atendente.servindoMesa();
-		//ação que somente o seu pacote cozinha precisa conhecer (default)
-		atendente.trocarGas();
+		
 		
 		Cliente cliente = new Cliente();
 		cliente.escolherLanche();
