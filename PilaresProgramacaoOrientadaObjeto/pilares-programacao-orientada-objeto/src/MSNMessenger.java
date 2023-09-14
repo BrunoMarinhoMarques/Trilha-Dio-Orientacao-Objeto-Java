4b69c06df571b6a61b8be30a510497e65680f1f7
Bruno Henrique Marinho Marques <brunohenriw1@gmail.com>
2023-09-14 13:55:33 -0300

    Encapsulamento dos métodos necessários.

--- PilaresProgramacaoOrientadaObjeto/pilares-programacao-orientada-objeto/src/MSNMessenger.java
+++ PilaresProgramacaoOrientadaObjeto/pilares-programacao-orientada-objeto/src/MSNMessenger.java
@@ -1,5 +1,16 @@
 public class MSNMessenger {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+	public void enviarMensagem() {
+        validarConectadoInternet();
+		System.out.println("Enviando mensagem");
+        salvarHistoricoMensagem();
+	}
+	public void receberMensagem() {
+		System.out.println("Recebendo mensagem");
+	}
+	private void validarConectadoInternet() {
+		System.out.println("Validando se está conectado a internet");
+	}
+	private void salvarHistoricoMensagem() {
+		System.out.println("Salvando o histórico da mensagem");
+	}
 }
