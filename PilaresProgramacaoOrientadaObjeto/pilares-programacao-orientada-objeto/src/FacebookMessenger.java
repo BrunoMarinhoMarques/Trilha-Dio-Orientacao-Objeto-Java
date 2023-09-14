b925ae346a70f7787e652fe1f06645219a3e8915
Bruno Henrique Marinho Marques <brunohenriw1@gmail.com>
2023-09-14 14:12:16 -0300

    Abstracao na classe pai e implantação classe filha

--- PilaresProgramacaoOrientadaObjeto/pilares-programacao-orientada-objeto/src/FacebookMessenger.java
+++ PilaresProgramacaoOrientadaObjeto/pilares-programacao-orientada-objeto/src/FacebookMessenger.java
@@ -1,3 +1,15 @@
 public class FacebookMessenger extends ServicoMensagemInstantanea {
+
+    @Override
+    public void enviarMensagem() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void receberMensagem() {
+        // TODO Auto-generated method stub
+        
+    }
     
 }
--- PilaresProgramacaoOrientadaObjeto/pilares-programacao-orientada-objeto/src/Telegram.java
+++ PilaresProgramacaoOrientadaObjeto/pilares-programacao-orientada-objeto/src/Telegram.java
@@ -1,3 +1,15 @@
 public class Telegram extends ServicoMensagemInstantanea{
+
+    @Override
+    public void enviarMensagem() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void receberMensagem() {
+        // TODO Auto-generated method stub
+        
+    }
     
 }
